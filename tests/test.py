60cbb1f407e97350fa39084dd89239cbb415948e
Salvo Lombardo <salvolombnardoj@gmail.com>
2025-09-19 17:47:16 +0200

    fix errore rimosso

---
+++ tests/test.py
@@ -0,0 +1,12 @@
+import pytest
+
+from services.weather_api import get_weather
+
+def test_service(city):
+    data=get_weather('Palermo')
+
+    
+
+    assert data["city"]=="Palermo"
+    assert "temp" in data
+    assert "description" in data
